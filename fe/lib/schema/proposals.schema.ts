292a63bda767dc6ba7841e01b81b8b73389aaf37
Theo in Tech <theoroque95@gmail.com>
2024-03-13 00:04:28 +0800

    Add proposal cards

---
+++ fe/lib/schema/proposals.schema.ts
@@ -0,0 +1,13 @@
+export enum EProposalStatus {
+  FINISHED = "Finished",
+  ONGOING = "Ongoing",
+  PENDING = "Pending",
+  FAILED = "Failed",
+}
+
+export enum EProposalType {
+  VOTE = "Vote",
+  BOUNTY = "Bounty",
+  EXECUTABLE = "Executable",
+  MULTIPLE_CHOICE = "Multiple Choice",
+}
