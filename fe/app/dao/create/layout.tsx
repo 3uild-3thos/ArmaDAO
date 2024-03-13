7c727767d5dafca7bd558799af5c3332237f84d7
Renzo Cabarios <renzo.cabarios@gmail.com>
2024-03-14 00:10:54 +0800

    Move create-dao to dao/create

---
+++ fe/app/dao/create/layout.tsx
@@ -0,0 +1,92 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { ChevronRight, Star } from "lucide-react";
+import { createContext, useContext, useState } from "react";
+
+export const CreateDaoContext = createContext<number>(0);
+
+const RootLayout = ({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) => {
+  const [page, setPage] = useState(0);
+
+  const next = () => {
+    setPage((state) => state + 1);
+  };
+
+  return (
+    <CreateDaoContext.Provider value={page}>
+      <div className="flex flex-col gap-5">
+        <p className="font-medium text-xl">Create a Project</p>
+
+        <div className="grid grid-cols-3 p-5 gap-10">
+          <CreateProjectStepper />
+          {children}
+        </div>
+        <div className="flex items-center justify-end">
+          <Button variant={"ghost"} onClick={next}>
+            Next
+          </Button>
+        </div>
+      </div>
+    </CreateDaoContext.Provider>
+  );
+};
+
+export default RootLayout;
+
+function CreateProjectStepper() {
+  const page = useContext(CreateDaoContext);
+
+  const STEPS = [
+    {
+      title: "SubDao Info",
+      description:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam ipsam aspernatur provident ullam ",
+    },
+    {
+      title: "Team Details",
+      description:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam ipsam aspernatur provident ullam ",
+    },
+    {
+      title: "SubDao Config",
+      description:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam ipsam aspernatur provident ullam ",
+    },
+    {
+      title: "Review / Confirmation",
+      description:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam ipsam aspernatur provident ullam ",
+    },
+  ];
+
+  return (
+    <div className="flex flex-col gap-20">
+      {STEPS.map((e: any, index: number) => (
+        <div className="grid grid-cols-10 gap-3" key={e.title}>
+          <div
+            className={`${
+              index == page && "rounded-full bg-gray-500"
+            } h-6 w-6 p-2 flex justify-center items-center`}
+          >
+            <p>{index + 1}</p>
+          </div>
+          <div className="flex justify-center">
+            <Star size={20} />
+          </div>
+          <div className="flex flex-col gap-2 col-span-7">
+            <p className="font-medium">{e.title}</p>
+            {/* TODO: Add proper text color */}
+            <p className="font-normal text-gray-500">{e.description}</p>
+          </div>
+          <div>
+            <ChevronRight size={30} />
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
