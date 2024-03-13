ea9bc9f3bec6ac8521ef2dc88614c10264659b58
Renzo Cabarios <renzo.cabarios@gmail.com>
2024-03-13 19:04:40 +0800

    Add icons

--- fe/app/create-dao/page.tsx
+++ fe/app/create-dao/page.tsx
@@ -1,103 +1,115 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { ChevronRight, ImagePlus, Star } from "lucide-react";
+
 export default function CreateDao() {
   return (
     <>
-      <p>Create a Project</p>
+      <div className="flex flex-col gap-10 p-10">
+        <p className="font-medium text-xl">Create a Project</p>
 
-      <div className="grid grid-cols-3 p-5">
-        <CreateProjectStepper />
-        <div className="col-span-2">
-          <div className="">
-            <p className="text-sm text-gray">Files</p>
-            <p className="">Cover Photo</p>
-            <p className="">
-              We suggest using 1200px (w) by 250px (h) with a 250px (w) by 250px
-              (h) safezone.
-            </p>
-
-            <div className="flex flex-col justify-center items-center gap-3">
-              <div className="">{/* TODO: Add image */}</div>
-              <div className="text-center">
-                <p className="">Click or drag an image here to upload.</p>
-                <p className="">Maximum file size: 5MB</p>
-              </div>
-            </div>
-          </div>
-
-          <div className="grid grid-cols-2 gap-4">
+        <div className="grid grid-cols-3 p-5 gap-10">
+          <CreateProjectStepper />
+          <div className="col-span-2 flex flex-col gap-10">
             <div className="flex flex-col gap-3">
-              <p>Basic Information</p>
+              <p className="text-sm text-gray-500">Files</p>
+              <p className="">Cover Photo</p>
+              {/* TODO: Add proper text color */}
+              <p className="text-xs font-light text-gray-500">
+                We suggest using 1200px (w) by 250px (h) with a 250px (w) by
+                250px (h) safezone.
+              </p>
 
-              <div className="flex gap-3 items-center">
-                <p>Title</p>
-                <input type="text" />
-              </div>
-              <div className="flex gap-3 items-center">
-                <p>Category</p>
-                <input type="text" />
-              </div>
-              <div className="flex gap-3 items-center">
+              {/* TODO: Add proper bg color */}
+              {/* TODO: Add proper border color */}
+              {/* TODO: Add proper border radius */}
+              <div className="border-dashed border-2 rounded border-gray-500 flex flex-col justify-center items-center gap-3 bg-card p-20 ">
                 <div className="">
-                  <p>Location</p>
-                  <p>Optional</p>
+                  <ImagePlus size={64} />
                 </div>
-                <input type="text" />
-              </div>
-
-              <div className="flex gap-3 items-center">
-                <p>Description</p>
-                <input type="text" />
+                <div className="text-center">
+                  <p className="">Click or drag an image here to upload.</p>
+                  <p className="">Maximum file size: 5MB</p>
+                </div>
               </div>
             </div>
 
-            <div className="flex flex-col gap-3">
-              <p>Links</p>
-              <div className="flex gap-3 items-center">
-                <p>Pitchdeck</p>
-                <input type="text" />
-              </div>
-              <div className="flex gap-3 items-center">
-                <div className="">
-                  <p>Demo Video</p>
-                  <p>Optional</p>
+            <div className="grid grid-cols-2 gap-10 ">
+              <div className="flex flex-col gap-7">
+                <p className="text-sm text-gray-500">Basic Information</p>
+                <div className="grid grid-cols-4 gap-3 items-center">
+                  <p>Title</p>
+                  <Input className="col-span-3" />
                 </div>
-                <input type="text" />
+                <div className="grid grid-cols-4 gap-3 items-center">
+                  <p>Category</p>
+                  <Input className="col-span-3" />
+                </div>
+                <div className="grid grid-cols-4 gap-3 items-center">
+                  <div className="flex flex-col gap-2">
+                    <p>Location</p>
+                    <p className="text-xs font-light text-gray-500">Optional</p>
+                  </div>
+                  <Input className="col-span-3" />
+                </div>
+
+                <div className="grid grid-cols-4 gap-3">
+                  <p>Description</p>
+                  <Textarea className="col-span-3" />
+                </div>
               </div>
 
-              <div className="flex gap-3 items-center">
-                <div className="">
-                  <p>Twitter / X</p>
-                  <p>Optional</p>
+              <div className="flex flex-col gap-7">
+                <p className="text-sm text-gray-500">Links</p>
+                <div className="grid grid-cols-4 gap-3 items-center">
+                  <p>Pitchdeck</p>
+                  <Input className="col-span-3" />
                 </div>
-                <input type="text" />
-              </div>
+                <div className="grid grid-cols-4 gap-3 items-center">
+                  <div className="flex flex-col gap-2">
+                    <p>Demo Video</p>
+                    <p className="text-xs font-light text-gray-500">Optional</p>
+                  </div>
+                  <Input className="col-span-3" />
+                </div>
 
-              <div className="flex gap-3 items-center">
-                <div className="">
-                  <p>Linked In</p>
-                  <p>Optional</p>
+                <div className="grid grid-cols-4 gap-3 items-center">
+                  <div className="flex flex-col gap-2">
+                    <p>Twitter / X</p>
+                    <p className="text-xs font-light text-gray-500">Optional</p>
+                  </div>
+                  <Input className="col-span-3" />
                 </div>
-                <input type="text" />
-              </div>
-              <div className="flex gap-3 items-center">
-                <div className="">
-                  <p>Github</p>
-                  <p>Optional</p>
+
+                <div className="grid grid-cols-4 gap-3 items-center">
+                  <div className="flex flex-col gap-2">
+                    <p>Linked In</p>
+                    <p className="text-xs font-light text-gray-500">Optional</p>
+                  </div>
+                  <Input className="col-span-3" />
                 </div>
-                <input type="text" />
-              </div>
-              <div className="flex gap-3 items-center">
-                <div className="">
-                  <p>Website</p>
-                  <p>Optional</p>
+                <div className="grid grid-cols-4 gap-3 items-center">
+                  <div className="flex flex-col gap-2">
+                    <p>Github</p>
+                    <p className="text-xs font-light text-gray-500">Optional</p>
+                  </div>
+                  <Input className="col-span-3" />
                 </div>
-                <input type="text" />
+                <div className="grid grid-cols-4 gap-3 items-center">
+                  <div className="flex flex-col gap-2">
+                    <p>Website</p>
+                    <p className="text-xs font-light text-gray-500">Optional</p>
+                  </div>
+                  <Input className="col-span-3" />
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-      <div className="flex items-center justify-end">
-        <button>Next</button>
+        <div className="flex items-center justify-end">
+          <Button variant={"ghost"}>Next</Button>
+        </div>
       </div>
     </>
   );
