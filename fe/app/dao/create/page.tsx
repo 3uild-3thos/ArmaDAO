64e4cb0d9668b315c2f2d9ad1a89b0be88313da0
Renzo Cabarios <renzo.cabarios@gmail.com>
2024-03-15 10:53:48 +0800

    Update aliases in tsconfig

--- fe/app/dao/create/page.tsx
+++ fe/app/dao/create/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 import { useContext } from "react";
-import { CreateDaoContext } from "./layout";
-import DaoInfo from "./(forms)/dao-info";
-import TeamInfo from "./(forms)/team-info";
+import { CreateDaoContext } from "@/create/layout";
+import DaoInfo from "@/create/(forms)/dao-info";
+import TeamInfo from "@/create/(forms)/team-info";
 
 export default function CreateDao() {
   const page = useContext(CreateDaoContext);
