708b48e4123f8045d8313afc62d709e8efb8a96b
Renzo Cabarios <renzo.cabarios@gmail.com>
2024-03-15 23:00:06 +0800

    Add team-member schema

---
+++ fe/app/dao/create/(forms)/team-member.tsx
@@ -0,0 +1,223 @@
+"use client";
+import { Textarea } from "@/components/ui/textarea";
+import { ImagePlus } from "lucide-react";
+import React from "react";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import {
+  ITeamMember,
+  TeamMemberDefaults,
+  TeamMemberSchema,
+} from "@/lib/schema/team-member.schema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
+
+function TeamMember() {
+  const form = useForm<ITeamMember>({
+    resolver: zodResolver(TeamMemberSchema),
+    defaultValues: TeamMemberDefaults,
+  });
+
+  function onSubmit(values: ITeamMember) {
+    console.log(values);
+  }
+
+  return (
+    <>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <div className="grid grid-cols-2 gap-8 border border-gray-500 py-3 px-4 rounded-xl">
+            <div className="col-span-2 flex items-center justify-between">
+              <p className="text-xl font-medium">Team Member</p>
+
+              <div className="flex items-center gap-3">
+                <p className="text-sm">Clear</p>
+                <p className="text-sm">Cancel</p>
+                <p className="text-sm">Done</p>
+              </div>
+            </div>
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <div className="flex flex-col justify-center gap-3">
+                      <p>Name</p>
+                      <Input
+                        className="col-span-3"
+                        placeholder="name"
+                        {...field}
+                      />
+                    </div>
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="position"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <div className="flex flex-col justify-center gap-3">
+                      <p>Position</p>
+                      <Input
+                        className="col-span-3"
+                        placeholder="position"
+                        {...field}
+                      />
+                    </div>
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="linkedIn"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <div className="flex flex-col justify-center gap-3">
+                      <p>LinkedIn Profile</p>
+                      <Input
+                        className="col-span-3"
+                        placeholder="linkedIn"
+                        {...field}
+                      />
+                    </div>
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="twitter"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <div className="flex flex-col justify-center gap-3">
+                      <p>Twitter / X Profile</p>
+                      <Input
+                        className="col-span-3"
+                        placeholder="twitter"
+                        {...field}
+                      />
+                    </div>
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="website"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <div className="flex flex-col justify-center gap-3">
+                      <div className="flex items-center gap-2">
+                        <p>Website</p>
+                        <p className="text-xs font-light text-gray-500">
+                          Optional
+                        </p>
+                      </div>
+                      <Input
+                        className="col-span-3"
+                        placeholder="website"
+                        {...field}
+                      />
+                    </div>
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="github"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <div className="flex flex-col justify-center gap-3">
+                      <div className="flex items-center gap-2">
+                        <p>Github Profile</p>
+                        <p className="text-xs font-light text-gray-500">
+                          Optional
+                        </p>
+                      </div>
+                      <Input
+                        className="col-span-3"
+                        placeholder="github"
+                        {...field}
+                      />
+                    </div>
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="description"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <div className="flex flex-col justify-center gap-3">
+                      <div className="flex items-center gap-2">
+                        <p>Description</p>
+                        <p className="text-xs font-light text-gray-500">
+                          Optional
+                        </p>
+                      </div>
+                      <Textarea
+                        className="col-span-3"
+                        placeholder="description"
+                        {...field}
+                      />
+                    </div>
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+
+            <div className="flex flex-col justify-center gap-3">
+              <div className="flex items-center gap-2">
+                <p>Profile Picture</p>
+                <p className="text-xs font-light text-gray-500">Optional</p>
+              </div>
+              <p className="text-xs font-light text-gray-500">
+                We suggest using 250px (w) by 250px (h) resolution.
+              </p>
+
+              <div className="border-dashed border-2 rounded border-gray-500 flex flex-col justify-center items-center gap-3 bg-card p-10 ">
+                <div className="">
+                  <ImagePlus size={50} />
+                </div>
+                <div className="text-center flex flex-col justify-center items-center gap-2">
+                  <p className="text-xs font-medium">
+                    Click or drag an image here to upload.
+                  </p>
+                  <p className="text-[10px] font-normal text-gray-500">
+                    Maximum file size: 5MB
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="flex justify-center">
+            <Button variant={"ghost"} type="submit">
+              Add a team member
+            </Button>
+          </div>
+        </form>
+      </Form>
+    </>
+  );
+}
+
+export default TeamMember;
--- fe/app/dao/create/layout.tsx
+++ fe/app/dao/create/layout.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { ChevronRight, Star } from "lucide-react";
-import { createContext, useContext, useState } from "react";
+import { createContext, useState } from "react";
 
 export const CreateDaoContext = createContext<number>(0);
 
