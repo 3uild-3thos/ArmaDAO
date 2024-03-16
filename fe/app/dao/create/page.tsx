d7230dd25acd7dad0f05fb4a3ecae3c0728ead87
Renzo Cabarios <renzo.cabarios@gmail.com>
2024-03-16 12:31:39 +0800

    Create config form

--- fe/app/dao/create/page.tsx
+++ fe/app/dao/create/page.tsx
@@ -3,10 +3,11 @@ import { useContext } from "react";
 import { CreateDaoContext } from "@/create/layout";
 import DaoInfo from "@/create/(forms)/dao-info";
 import TeamInfo from "@/create/(forms)/team-info";
-
+import Config from "@/create/(forms)/config";
 export default function CreateDao() {
   const page = useContext(CreateDaoContext);
 
   if (page === 1) return <TeamInfo />;
+  if (page === 2) return <Config />;
   return <DaoInfo />;
 }
