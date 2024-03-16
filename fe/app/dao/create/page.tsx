b4d84e549ad8874ad188eda1fe7047051f93037d
Renzo Cabarios <renzo.cabarios@gmail.com>
2024-03-17 00:27:00 +0800

    Refactor CreateDao

--- fe/app/dao/create/page.tsx
+++ fe/app/dao/create/page.tsx
@@ -4,10 +4,18 @@ import { CreateDaoContext } from "@/create/layout";
 import DaoInfo from "@/create/(forms)/dao-info";
 import TeamInfo from "@/create/(forms)/team-info";
 import Config from "@/create/(forms)/config";
+
 export default function CreateDao() {
   const page = useContext(CreateDaoContext);
-
-  if (page === 1) return <TeamInfo />;
-  if (page === 2) return <Config />;
-  return <DaoInfo />;
+  switch (page) {
+    case 1: {
+      return <TeamInfo />;
+    }
+    case 2: {
+      return <Config />;
+    }
+    default: {
+      return <DaoInfo />;
+    }
+  }
 }
