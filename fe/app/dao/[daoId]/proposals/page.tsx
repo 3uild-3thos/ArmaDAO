5998fc79d2f4fa72b0b82acede9d1777b6a54414
Theo in Tech <theoroque95@gmail.com>
2024-03-18 20:40:33 +0800

    Add no data state for proposals

--- fe/app/dao/[daoId]/proposals/page.tsx
+++ fe/app/dao/[daoId]/proposals/page.tsx
@@ -1,14 +1,33 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
 import { proposals } from "@/mock/proposals";
 import ProposalCard from "@/proposals/proposal-card";
+import { AlertCircle } from "lucide-react";
 
 const DAOProposalPage = () => {
   return (
     <div className="grid grid-cols-12">
       <div className="flex flex-col col-span-8 gap-8">
         <h3 className="text-2xl text-muted">Proposals</h3>
-        {proposals.map((proposal) => (
-          <ProposalCard key={`proposal-${proposal.id}`} {...proposal} />
-        ))}
+        {proposals.length === 0 ? (
+          <Card>
+            <CardContent className="flex flex-col items-center justify-center gap-4 min-h-[24rem]">
+              <AlertCircle size={32} className="text-muted-light" />
+              <div className="text-muted-light">
+                No proposals yet. Start one now!
+              </div>
+              <Button variant={"white"} className="mt-8">
+                Create Proposal
+              </Button>
+            </CardContent>
+          </Card>
+        ) : (
+          <>
+            {proposals.map((proposal) => (
+              <ProposalCard key={`proposal-${proposal.id}`} {...proposal} />
+            ))}
+          </>
+        )}
       </div>
       <div className="flex flex-col col-span-4 gap-8"></div>
     </div>
