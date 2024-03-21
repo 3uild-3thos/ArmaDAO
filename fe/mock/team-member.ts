9d734f95225ab5188c69ba7a1332e47b272ae1d4
Renzo Cabarios <renzo.cabarios@gmail.com>
2024-03-22 02:10:58 +0800

    Add teammember mock

---
+++ fe/mock/team-member.ts
@@ -0,0 +1,27 @@
+import { ITeamMember } from "@/lib/schema/team-member.schema";
+
+export const teamMembers: Array<ITeamMember> = [
+  {
+    id: "1",
+    name: "John Doe",
+    position: "CEO & Co-Founder",
+    linkedIn: "https://www.linkedin.com/feed/",
+    twitter: "https://twitter.com/home",
+    website: "https://twitter.com/TheArmadaDAO",
+    github: "https://github.com/",
+    category: "",
+    description:
+      "This is a short description or story of this team member. This is a short description or story of this team member. This is a short description or story of this team member. This is a short description or story of this team member. This is a short description or story of this team member.",
+  },
+  {
+    id: "2",
+    name: "Tom Whitman",
+    position: "CFO & Co-Founder",
+    linkedIn: "https://www.linkedin.com/feed/",
+    twitter: "https://twitter.com/home",
+    website: "https://twitter.com/TheArmadaDAO",
+    github: "https://github.com/",
+    category: "",
+    description: "",
+  },
+];
