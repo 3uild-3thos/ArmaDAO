3c4de27097b3f44f96293a743a8868185965ab93
Renzo Cabarios <renzo.cabarios@gmail.com>
2024-03-28 00:04:59 +0800

    Update next config

--- fe/next.config.js
+++ fe/next.config.js
@@ -3,11 +3,19 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   transpilePackages: ["@acme/ui"],
-  webpack(config) {
+  webpack(config, context) {
     config.module.rules.push({
       test: /\.svg$/,
       use: ["@svgr/webpack"],
     });
+
+    if (config.plugins) {
+      config.plugins.push(
+        new context.webpack.IgnorePlugin({
+          resourceRegExp: /^(lokijs|pino-pretty|encoding)$/,
+        })
+      );
+    }
 
     return config;
   },
