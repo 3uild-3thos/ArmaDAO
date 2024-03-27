e49f92b68072713be5d500ba175018f10628c6f0
Renzo Cabarios <renzo.cabarios@gmail.com>
2024-03-27 21:50:00 +0800

    Clean

--- fe/app/dao/subdao-carousel.tsx
+++ fe/app/dao/subdao-carousel.tsx
@@ -1,9 +1,3 @@
-"use client";
-import { useState } from "react";
-
-// next
-import Image from "next/image";
-
 // components
 import {
   Carousel,
@@ -12,15 +6,10 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
-import { Coins, Newspaper, Users } from "lucide-react";
-import { Button } from "@/components/ui/button";
-
-// lib
-import shortenDescription from "@/lib/helpers/shortenDescription";
-import { ISubDaoInfo } from "@/lib/schema/subdao-info.schema";
 
 // mock
 import { daos } from "@/mock/daos";
+import SubdaoCard from "@/dao/subdao-card";
 
 function SubDaoCarouselList() {
   return (
@@ -31,7 +20,7 @@ function SubDaoCarouselList() {
           {daos.map((dao) => {
             return (
               <CarouselItem key={dao.id} className="basis-1/5">
-                <SubdaoCard dao={dao} />
+                <SubdaoCard dao={dao} owned={true} />
               </CarouselItem>
             );
           })}
@@ -39,81 +28,6 @@ function SubDaoCarouselList() {
         <CarouselPrevious />
         <CarouselNext />
       </Carousel>
-    </div>
-  );
-}
-
-interface ISubdaoCardProps {
-  dao: ISubDaoInfo;
-}
-
-function SubdaoCard({ dao }: ISubdaoCardProps) {
-  const [seeMore, setseeMore] = useState(false);
-
-  const onSeeMore = () => {
-    setseeMore((state) => !state);
-  };
-
-  return (
-    <div className="flex flex-col gap-3 border rounded-lg border-gray-600">
-      <div className="flex flex-col justify-center items-center">
-        <div className="h-full w-full">
-          <Image
-            src={dao.image ?? ""}
-            alt={dao.image ?? ""}
-            width={300}
-            height={300}
-            className="rounded-t-lg  object-cover shadow-2xl"
-          />
-        </div>
-      </div>
-
-      <div className="col-span-2 flex flex-col gap-1 p-3">
-        <div className="flex flex-col gap-4">
-          <div className="flex justify-between items-center">
-            <p className="font-medium text-xl">{dao.title}</p>
-          </div>
-
-          <div className="flex flex-wrap gap-3 items-center">
-            <p className=" text-xs">
-              {seeMore
-                ? shortenDescription(dao.description, 95)
-                : shortenDescription(dao.description, 30)}
-            </p>
-
-            {dao.description.length >= 50 && (
-              <Button
-                variant={"ghost"}
-                className="p-0 m-0 h-0"
-                onClick={onSeeMore}
-              >
-                <p className="font-medium text-xs">
-                  {seeMore ? "See less" : "See more"}
-                </p>
-              </Button>
-            )}
-          </div>
-
-          <div className="flex items-center justify-between">
-            <div className="flex items-center gap-2">
-              <div className="bg-gray-800 p-1 rounded-full">
-                <Coins size={16} />
-              </div>
-              <div className="bg-gray-800 p-1 rounded-full">
-                <Users size={16} />
-              </div>
-              <p className="text-[10px] font-medium">{dao.members}</p>
-            </div>
-
-            <div className="flex items-center gap-2">
-              <div className="bg-gray-800 p-1 rounded-full">
-                <Newspaper size={16} />
-              </div>
-              <p className="text-[10px] font-medium">{dao.members}</p>
-            </div>
-          </div>
-        </div>
-      </div>
     </div>
   );
 }
--- fe/app/dao/subdaos-list.tsx
+++ fe/app/dao/subdaos-list.tsx
@@ -1,107 +1,17 @@
-"use client";
-import React, { useState } from "react";
-
-// next
-import Image from "next/image";
-
-// components
-import { Button } from "@/components/ui/button";
-import { Coins, MoveRight, Newspaper, Users } from "lucide-react";
-
-// lip
-import { ISubDaoInfo } from "@/lib/schema/subdao-info.schema";
-import shortenDescription from "@/lib/helpers/shortenDescription";
-
 // mock
 import { daos } from "@/mock/daos";
+
+// components
+import SubdaoCard from "@/dao/subdao-card";
 
 function SubDaosList() {
   return (
     <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
       {daos.map((dao) => {
-        return <ProjectCard key={dao.id} dao={dao} />;
+        return <SubdaoCard key={dao.id} dao={dao} />;
       })}
     </div>
   );
 }
 
 export default SubDaosList;
-
-interface IProjectCartProps {
-  dao: ISubDaoInfo;
-}
-
-function ProjectCard({ dao }: IProjectCartProps) {
-  const [seeMore, setseeMore] = useState(false);
-
-  const onSeeMore = () => {
-    setseeMore((state) => !state);
-  };
-
-  return (
-    <div className="flex flex-col p-4 gap-5 border border-gray-500 rounded-lg max-w-96">
-      <div className="w-full flex justify-center items-center">
-        <div className="h-fit w-fit">
-          <Image
-            src={dao.image ?? ""}
-            alt={dao.image ?? ""}
-            width={256}
-            height={250}
-            className="rounded-lg object-cover shadow-2xl"
-          />
-        </div>
-      </div>
-
-      <div className="flex flex-col gap-4">
-        <p className="font-medium text-xl">{dao.title}</p>
-        <p className="font-medium text-xs">By {dao.creator}</p>
-      </div>
-
-      <div className="flex flex-wrap gap-3 items-center">
-        <p className=" text-xs">
-          {seeMore
-            ? shortenDescription(dao.description, 170)
-            : shortenDescription(dao.description, 40)}
-        </p>
-
-        {dao.description.length >= 50 && (
-          <Button variant={"ghost"} className="p-0 m-0 h-0" onClick={onSeeMore}>
-            <p className="font-medium text-xs">
-              {seeMore ? "See less" : "See more"}
-            </p>
-          </Button>
-        )}
-      </div>
-
-      <div className="flex items-center justify-between">
-        <div className="flex items-center gap-2">
-          <div className="bg-gray-800 p-1 rounded-full">
-            <Coins size={16} />
-          </div>
-          <div className="bg-gray-800 p-1 rounded-full">
-            <Users size={16} />
-          </div>
-          <p className="text-[10px] font-medium">{dao.members}</p>
-        </div>
-
-        <div className="flex items-center gap-2">
-          <div className="bg-gray-800 p-1 rounded-full">
-            <Newspaper size={16} />
-          </div>
-          <p className="text-[10px] font-medium">{dao.members}</p>
-        </div>
-      </div>
-
-      <Button
-        variant={"outline"}
-        className="w-full flex items-center gap-2"
-        onClick={() => {
-          alert("Join the party");
-        }}
-      >
-        <p className="font-medium text-sm">Join</p>
-        <MoveRight size={16} />
-      </Button>
-    </div>
-  );
-}
