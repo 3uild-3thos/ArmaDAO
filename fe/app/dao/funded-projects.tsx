534993f6d6301bb300bc67b023d6ae9dee2b36a3
Renzo Cabarios <renzo.cabarios@gmail.com>
2024-03-27 21:49:57 +0800

    Update FundedProjects

--- fe/app/dao/funded-projects.tsx
+++ fe/app/dao/funded-projects.tsx
@@ -12,33 +12,27 @@ import {
 import { Progress } from "@/components/ui/progress";
 import { Bell, ThumbsUp } from "lucide-react";
 
+// lib
+import { IProject } from "@/lib/schema/projects.schema";
+import getRemainingDateTime from "@/lib/helpers/getRemainingDateTime";
+import getPercentage from "@/lib/helpers/getPercentage";
+
+// mock
+import { projects } from "@/mock/projects";
+
 function FundedProjects() {
   return (
     <div className="flex flex-col gap-4">
       <p className="text-xs font-medium">Your funded projects</p>
       <Carousel>
         <CarouselContent className="ml-4">
-          <CarouselItem className="basis-1/3">
-            <FundedProjectCard />
-          </CarouselItem>
-          <CarouselItem className="basis-1/3">
-            <FundedProjectCard />
-          </CarouselItem>
-          <CarouselItem className="basis-1/3">
-            <FundedProjectCard />
-          </CarouselItem>
-          <CarouselItem className="basis-1/3">
-            <FundedProjectCard />
-          </CarouselItem>
-          <CarouselItem className="basis-1/3">
-            <FundedProjectCard />
-          </CarouselItem>
-          <CarouselItem className="basis-1/3">
-            <FundedProjectCard />
-          </CarouselItem>
-          <CarouselItem className="basis-1/3">
-            <FundedProjectCard />
-          </CarouselItem>
+          {projects.map((project) => {
+            return (
+              <CarouselItem key={project.id} className="basis-1/3">
+                <FundedProjectCard project={project} />
+              </CarouselItem>
+            );
+          })}
         </CarouselContent>
         <CarouselPrevious />
         <CarouselNext />
@@ -47,14 +41,18 @@ function FundedProjects() {
   );
 }
 
-function FundedProjectCard() {
+interface IFundedProjectCardProps {
+  project: IProject;
+}
+
+function FundedProjectCard({ project }: IFundedProjectCardProps) {
   return (
     <div className="grid grid-cols-3 p-4 gap-6">
       <div className="flex justify-center items-center">
         <div className="h-fit w-fit">
           <Image
-            src={"https://placehold.co/145x145"}
-            alt={"DAO Banner"}
+            src={project.image}
+            alt={project.image}
             width={145}
             height={145}
             className="rounded-lg object-cover shadow-2xl"
@@ -65,7 +63,7 @@ function FundedProjectCard() {
       <div className="col-span-2 flex flex-col gap-4">
         <div className="flex flex-col gap-2">
           <div className="flex justify-between items-center">
-            <p className="font-medium text-xl">DAOCre-8</p>
+            <p className="font-medium text-xl">{project.name}</p>
 
             <div className="flex items-center gap-2">
               <ThumbsUp size={20} />
@@ -73,33 +71,43 @@ function FundedProjectCard() {
             </div>
           </div>
 
-          <p className="font-medium text-xs">By DAOCre8 Inc.</p>
+          <p className="font-medium text-xs">By {project.creator}</p>
           <div className="flex gap-4">
             <div className="flex flex-col gap-2">
               <p className="text-[10px]">Type</p>
-              <p className="text-xs font-medium">Software</p>
+              <p className="text-xs font-medium">{project.type}</p>
             </div>
 
             <div className="flex flex-col gap-2">
               <p className="text-[10px]">Backers</p>
-              <p className="text-xs font-medium">1,784</p>
+              {/* TODO: Add "," to numbers */}
+              <p className="text-xs font-medium">{project.backers}</p>
             </div>
 
             <div className="flex flex-col gap-2">
               <p className="text-[10px]">Ends in</p>
-              <p className="text-xs font-medium">26d 5h 34m 42s</p>
+              <p className="text-xs font-medium">
+                {getRemainingDateTime(project.expiry)}
+              </p>
             </div>
           </div>
         </div>
         <div className="flex flex-col gap-2">
-          <Progress value={32.41} className="h-[2px]" />
+          <Progress
+            value={getPercentage(project.currentFunds, project.goal)}
+            className="h-[2px]"
+          />
           <div className="flex justify-between">
             <div className="flex gap-1 items-center">
-              <p className="text-sm font-medium">$ 32,412</p>
-              <p className="text-[10px]">/ 100,000 goal</p>
+              {/* TODO: Add "," to numbers */}
+              <p className="text-sm font-medium">$ {project.currentFunds}</p>
+              {/* TODO: Add "," to numbers */}
+              <p className="text-[10px]">/ {project.goal} goal</p>
             </div>
 
-            <p className="text-[10px]">32.41% Funded</p>
+            <p className="text-[10px]">
+              {getPercentage(project.currentFunds, project.goal)}% Funded
+            </p>
           </div>
         </div>
       </div>
