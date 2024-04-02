a7e274c94d388ee2552cd99bab0f26d141eb9324
Renzo Cabarios <renzo.cabarios@gmail.com>
2024-04-02 18:19:32 +0800

    Update blockchain config

--- fe/lib/blockchain-config.ts
+++ fe/lib/blockchain-config.ts
@@ -17,10 +17,10 @@ interface IBlockchainConfig {
 
 const config: IBlockchainConfig = {
   devnet: {
-    proposalProgram: "propm845StqEBV57ZSnTe8EW8duzAxo5p7h4inhibXV",
-    stakingProgram: "propm845StqEBV57ZSnTe8EW8duzAxo5p7h4inhibXV",
+    proposalProgram: "daoSYkGVA6pu5CxknvVMMTc8nFAGsYzfQt2jK5CgC5V",
+    stakingProgram: "stakyTBmEpbUcxNhjiv16Bvr53RVy68ENBZXPiUzNcF",
     daoProgram: "propm845StqEBV57ZSnTe8EW8duzAxo5p7h4inhibXV",
-    votingProgram: "propm845StqEBV57ZSnTe8EW8duzAxo5p7h4inhibXV",
+    votingProgram: "voteUXym9t6h3VzHYumMyMCXmiDoYqMiC2g4JnvdnGC",
     cluster: WalletAdapterNetwork.Devnet,
     rpcUrl: clusterApiUrl(WalletAdapterNetwork.Devnet),
     connection: new Connection(
