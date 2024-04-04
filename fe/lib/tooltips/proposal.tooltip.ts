f4ab6468c1bb73a38255695c5d7c391dd2aa5807
Theo in Tech <theoroque95@gmail.com>
2024-04-05 07:37:21 +0800

    Add tooltip for proposals

---
+++ fe/lib/tooltips/proposal.tooltip.ts
@@ -0,0 +1,8 @@
+export const proposal = {
+  proposalTypes: {
+    vote: `Vote on questions that doesn't change the config or state of the DAO.`,
+    bounty: `Propose a bounty you will be working on and get rewarded when it's completed.`,
+    executable: `Change the config of the DAO by executing a programmatic action.`,
+    multichoice: `Propose a task or a question and let the members vote on the best bounty or vote.`,
+  },
+};
