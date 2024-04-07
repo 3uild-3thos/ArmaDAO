12a1055549a9902f6c8e4dfa8ada5df831b993e4
Jony <110206718+belivenn@users.noreply.github.com>
2024-04-07 16:39:26 +0100

    Delete DAO_Programs/crates/daoist-programs directory
    Updated

--- DAO_Programs/crates/daoist-programs/src/lib.rs
+++
@@ -1,3 +0,0 @@
-pub mod modules;
-mod errors;
-mod constants;
--- DAO_Programs/crates/daoist-programs/src/modules/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod proposal_program;
-pub mod staking_program;
-pub mod core_program;
-
-pub use core_program::*;
-pub use staking_program::*;
-pub use proposal_program::*;
